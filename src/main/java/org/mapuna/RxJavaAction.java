8f0095474906ff804a33d0ba43aad0722117ab72
Anupam Gupta <mapuna@gmail.com>
2018-08-10 20:20:57 +0530

    Added a few more examples.

--- src/main/java/org/mapuna/RxJavaAction.java
+++ src/main/java/org/mapuna/RxJavaAction.java
@@ -1,7 +1,12 @@
 package org.mapuna;
 
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxJavaAction {
     public static void main(String[] args) {
@@ -12,9 +17,43 @@ public class RxJavaAction {
         );
 
         hello("Anupam", "Priya", "Lavanya");
+
+        Maybe.just(1)
+                //.map(v -> v + 1)
+                .filter(v -> v == 1)
+                .defaultIfEmpty(2)
+                .test()
+                .assertResult(1);
+
+        Observable.just("Ambujam", "Anupam", "Priya", "Goofy", "Lavanya")
+                .observeOn(Schedulers.io())
+                .subscribe(
+                        new Observer<String>() {
+                            @Override
+                            public void onSubscribe(Disposable d) {
+                                if (!d.isDisposed())
+                                    System.out.println("Subscribed");
+                            }
+
+                            @Override
+                            public void onNext(String s) {
+                                System.out.println(s);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                System.err.println(e.getMessage());
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                System.out.println("Completed");
+                            }
+                        }
+                );
     }
 
-    public static void hello(String... names) {
+    private static void hello(String... names) {
         Observable.fromArray(names).subscribe(new Consumer<String>() {
 
             @Override
