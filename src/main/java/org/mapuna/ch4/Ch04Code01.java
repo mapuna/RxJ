237f258f084e0744010aec44359f39f8348ea8a5
Anupam Gupta <mapuna@gmail.com>
2018-08-18 19:36:30 +0530

    Added code on specialized streams, e.g., Single, Flowable etc.

---
+++ src/main/java/org/mapuna/ch4/Ch04Code01.java
@@ -0,0 +1,19 @@
+package org.mapuna.ch4;
+
+import io.reactivex.Single;
+
+/**
+ * A `Single' is a specialized stream that only emits one item. It works like the
+ * `Observable' streams we have seen previously but is limited to operators that
+ * make sense for a single emission.
+ *
+ * Typically, doOnNext and doOnComplete are replaced by doOnSuccess that accept
+ * the produced item.
+ */
+public class Ch04Code01 {
+    public static void main(String[] args) {
+        Single.just("Thanos")
+                .doOnSuccess(s -> System.out.println("Hello, " + s + "!"))
+                .subscribe();
+    }
+}
